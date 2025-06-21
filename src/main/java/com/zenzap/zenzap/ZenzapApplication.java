4892d9aa75ab1fdf2c09e0ca50740b9744cfb8de
Michelle-RA <michelleramirezagudelo@gmail.com>
2025-06-20 20:04:47 -0500

    validacion de usuario y contraseña

---
+++ src/main/java/com/zenzap/zenzap/ZenzapApplication.java
@@ -0,0 +1,12 @@
+package com.zenzap.zenzap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenzapApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZenzapApplication.class, args);
+	}
+
+}
